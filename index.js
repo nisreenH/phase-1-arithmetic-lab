a9083ab2418f2fd4b8c38d2ceb825b1cb9fcb509
Nisreen Hamzah <nisreenhamzah138@gmail.com>
2022-10-20 15:26:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1=1;
+const num2 = 62;
 
-// Write your code here
+const num3=24;
+const num4 = 5;
+
+const multiply = num1 * num2;
+
+const random = parseInt(Math.random() + 1);
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 3, 4, 20);
